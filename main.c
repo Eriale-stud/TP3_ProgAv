233608e3613d61987d0df2e872c4591b8498cbc6
Eriale Nathan <wordwars@hotmail.fr>
2020-04-08 23:30:14 +0000

    test

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ typedef struct{
     char ** Case;
 }matrice;
 
+void setCase(matrice * m, int l, int c);
+
 //Fonction initialisant un "monde" a vide en prenant un monde en parametre ainsi que ses dimensions
 void initEmptyQueen (matrice * m){
 
@@ -17,23 +19,43 @@ void initEmptyQueen (matrice * m){
 
     //Allocation de la memoire pour le tableau qui en "accueillera"
     //d autre creant ainsi une matrice
-    (*m).Case = malloc(L * sizeof (char*));
+    (*m).Case = malloc((*m).lin * sizeof (char*));
 
 
-    //Allocation de la memoire des tableaux definissant ainsi les lignes de notre matrice "monde"
-    for(i=0;i<L;i++){
-        (*m).Case[i] = malloc(C * sizeof(char*));
-        for (j=0;j<C;j++){
-            setCase(m,i+1,j+1,' '); //Voir le partie pris dans la fonction setCell pour expliquer ces "i+1" et "j+1"
+    //Allocation de la memoire des tableaux definissant ainsi les lignes de notre "echiquier"
+    for(i=0;i<(*m).lin;i++){
+        (*m).Case[i] = malloc((*m).col * sizeof(char*));
+        for (j=0;j<(*m).col;j++){
+            setCase(m,i+1,j+1,' '); //Voir le partie pris dans la fonction setCase pour expliquer ces "i+1" et "j+1"
         }
     }
+}
+
+//Fonction affichant le "echiquier" dans le terminal
+void print(matrice * m){
+  int i,j;
+  system("cls");                       //efface le contenu de la console permettant un affichage moins pollue
+  for(i=0;i<=((*m).lin)-1;i++){         //on parcours chaque ligne du monde
+    for(j=0;j<=((*m).col)-1;j++){             //on parcours chaque colonne du monde
+      printf("|");
+      printf("%c",(*m).Case[i][j]);   //on affiche la cellule de coordonee (i,j)
+    }
+    printf("|\n");
+  }
+  printf("\n");
+}
+
+//Fonction inserant(remplace la valeur actuel) une reine a une position [i,j] de notre "echiquier"
+void setCase(matrice * m, int l, int c, char ){
+    (*m).Case[l-1][c-1] = 'x'; //Partie pris permettant une utilisation "plus simple" de la fonction celon le contexte
 }
 
 int main(void) {
   int * solution[8];
   matrice m; 
 
-    
+  initEmptyQueen(&m);
+  print(&m);   
 
   return 0;
 }
